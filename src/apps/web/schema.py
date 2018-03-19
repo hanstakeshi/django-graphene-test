9e06f221098dbe1e3b8b413c7f1647a74a0f59f6
hanstakeshi <agurtohans@gmail.com>
2018-03-19 09:12:29 -0500

    django-filter comment sintaxys

--- src/apps/web/schema.py
+++ src/apps/web/schema.py
@@ -7,12 +7,40 @@ from graphene_django.filter import DjangoFilterConnectionField
 from . import models
 
 
+# Consulta Simple
 class MensajeType(DjangoObjectType):
+    class Meta:
+        model = models.Mensaje
+    """
+    {
+      allMessages{
+        mensaje, id
+      }
+    }
+    """
+
+# Consulta filtrada
+class MensajeTypeFilter(DjangoObjectType):
     class Meta:
         model = models.Mensaje
         filter_fields = {'mensaje': ['icontains']}
         interfaces = (graphene.relay.Node, )
+    """
+    {
+      filterMessages(mensaje_Icontains:"1"){
+        edges{
+          node{
+            id, mensaje
+          }
+        }
+      }
+    }
 
+    """
 
 class Query(graphene.AbstractType):
-    all_messages = DjangoFilterConnectionField(MensajeType)
+    filter_messages = DjangoFilterConnectionField(MensajeTypeFilter)
+    all_messages = graphene.List(MensajeType)
+
+    def resolve_all_messages(self, args):
+        return models.Mensaje.objects.all()
